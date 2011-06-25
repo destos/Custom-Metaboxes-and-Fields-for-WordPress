d416f4481c78be5698a57fab39cc21e35b5e27cf
Patrick Forringer <patrick@forringer.com>
2011-06-24 21:18:36 -0500

    Moving the metabox creation loop out of init.php

--- init.php
+++ init.php
@@ -32,28 +32,7 @@ Version: 		0.4
 		You should not edit the code below or things might explode!
 *************************************************************************/
 
-foreach ( $meta_boxes as $meta_box ) {
-	$my_box = new cmb_Meta_Box($meta_box);
-}
-
-
 /**
- * Validate value of meta fields
- * Define ALL validation methods inside this class and use the names of these 
- * methods in the definition of meta boxes (key 'validate_func' of each field)
- */
-
-class cmb_Meta_Box_Validate {
-	function check_text( $text ) {
-		if ($text != 'hello') {
-			return false;
-		}
-		return true;
-	}
-}
-
-
-/*
  * url to load local resources.
  */
 
